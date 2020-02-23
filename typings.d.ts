2e66e51da4083c54fed7923b548d65eeef5d9dd4
paulincai <paulailincai@icloud.com>
2020-02-24 03:46:25 +0400

    Removed useless dependencies

--- typings.d.ts
+++ typings.d.ts
@@ -1,35 +1,35 @@
-import { ComponentClass } from "react";
+import { ComponentClass } from "react"
 
 export interface MasonryOptions {
-    columnWidth?: number | string | HTMLElement | null;
-    itemSelector?: string;
-    gutter?: number | string;
-    percentPosition?: boolean;
-    horizontalOrder?: boolean;
-    stamp?: string;
-    fitWidth?: boolean;
-    originLeft?: boolean;
-    originTop?: boolean;
-    containerStyle?: Object;
-    transitionDuration?: number | string;
-    resize?: boolean;
-    initLayout?: boolean;
+    columnWidth?: number | string | HTMLElement | null
+    itemSelector?: string
+    gutter?: number | string
+    percentPosition?: boolean
+    horizontalOrder?: boolean
+    stamp?: string
+    fitWidth?: boolean
+    originLeft?: boolean
+    originTop?: boolean
+    containerStyle?: Object
+    transitionDuration?: number | string
+    resize?: boolean
+    initLayout?: boolean
 }
 
 export interface MasonryPropTypes {
-    enableResizableChildren?: boolean;
-    disableImagesLoaded?: boolean;
-    updateOnEachImageLoad?: boolean;
-    onImagesLoaded?: (instance: any) => void;
-    options?: MasonryOptions;
-    className?: string;
-    elementType?: string;
-    style?: Object;
-    onLayoutComplete?: (instance: any) => void;
-    onRemoveComplete?: (instance: any) => void;
-    updateOnEachComponentUpdate?: boolean;
+    enableResizableChildren?: boolean
+    disableImagesLoaded?: boolean
+    updateOnEachImageLoad?: boolean
+    onImagesLoaded?: (instance: any) => void
+    options?: MasonryOptions
+    className?: string
+    elementType?: string
+    style?: Object
+    onLayoutComplete?: (instance: any) => void
+    onRemoveComplete?: (instance: any) => void
+    updateOnEachComponentUpdate?: boolean
 }
 
-declare const Masonry: ComponentClass<MasonryPropTypes>;
-export as namespace Masonry;
-export default Masonry;
+declare const Masonry: ComponentClass<MasonryPropTypes>
+export as namespace Masonry
+export default Masonry
